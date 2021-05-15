9dadd852b69b7882577c0ab6ac7f42f539d9c58a
Sam Free5ide <snovvcrash@protonmail.ch>
2021-05-16 02:47:32 +0300

    Some changes
    - Switch over to [pyyed] [1] library instead of networkx for generating GraphML
    - Migrate Python 2 code to Python 3
    - Stick to one casing style (lowercase) for node labels (#1)
    - Add more trust attributes (#2)
    
    [1]: https://github.com/jamesscottbrown/pyyed

--- TrustVisualizer.py
+++ TrustVisualizer.py
@@ -1,86 +1,90 @@
-#!/usr/bin/python
+#!/usr/bin/env python3
 
 ##############################################################################
 # Author: @harmj0y
 #
 # Based on: https://github.com/sixdub/DomainTrustExplorer by @sixdub
 #
-# Description: Usesnetworkx library to transform PowerView's updated 
+# Description: Uses pyyed (yEd) library to transform PowerView's updated
 #              Get-DomainTrustMapping functionality output to graphml
 #
 # License: BSD 3-clause
 ##############################################################################
 
-import networkx as nx
-import sys, csv
+import csv
+from os.path import basename, splitext
+from argparse import ArgumentParser
+
+import pyyed
+
+parser = ArgumentParser()
+parser.add_argument('trust_file', help='trust file in .csv format (generate with PowerView)')
+args = parser.parse_args()
 
 if __name__ == '__main__':
+	graph = pyyed.Graph()
 
-    if (len(sys.argv) != 2):
-        print "usage: ./TrustVisualizer.py <trust_file.csv>"
-        exit()
+	with open(args.trust_file, 'r', encoding='utf-8') as fd:
+		reader = csv.reader(fd, delimiter=',')
+		next(reader, None)
 
-    graph = nx.DiGraph()
-    intputFile = sys.argv[1]
+		for row in reader:
+			# csv format:
+			#   "SourceName","TargetName","TrustType","TrustAttributes","TrustDirection","WhenCreated","WhenChanged"
+			sourceName = row[0].strip().lower()
+			targetName = row[1].strip().lower()
+			trustType = row[2].strip()
+			trustAttributes = row[3].strip()
+			trustDirection = row[4].strip()
 
-    with open(intputFile, 'rb') as csvfile:
+			# if the source and destination domains are the same, skip
+			if sourceName == targetName:
+				continue
 
-        reader = csv.reader(csvfile, delimiter=',')
+			if trustType == 'MIT':
+				# black label for MIT trusts
+				ecolor = '#000000'
+			else:
+				if 'WITHIN_FOREST' in trustAttributes:
+					# green label for intra-forest trusts
+					ecolor = '#009900'
+				elif 'FOREST_TRANSITIVE' in trustAttributes:
+					# blue label for inter-forest trusts
+					ecolor = '#0000CC'
+				elif trustAttributes == '' or any(attr in trustAttributes for attr in ('TREAT_AS_EXTERNAL', 'FILTER_SIDS', 'CROSS_ORGANIZATION')):
+					# red label for external trusts
+					ecolor = '#FF0000'
+				else:
+					# violet label for unknown
+					print(f'[-] Unrecognized trust attributes between {sourceName} and {targetName} : {trustAttributes}')
+					ecolor = '#EE82EE'
 
-        for row in reader:
+			try:
+				# add source node to the internal graph
+				graph.add_node(sourceName, label=sourceName, shape_fill='#FFCC00')
+			# node exists
+			except RuntimeWarning:
+				pass
 
-            # if we have the header row, skip
-            if row[0] == 'SourceName':
-                continue
+			try:
+				# add target node to the internal graph
+				graph.add_node(targetName, label=targetName, shape_fill='#FFCC00')
+			# node exists
+			except RuntimeWarning:
+				pass
 
-            # csv format:
-            #   SourceName,TargetName,TrustType,TrustAttributes,TrustDirection,WhenCreated,WhenChanged
-            ecolor = ''
-            sourceName = row[0].strip()
-            targetName = row[1].strip()
-            trustType = row[2].strip()
-            trustAttributes = row[3].strip()
-            trustDirection = row[4].strip()
+			# add the edges to the graph
+			if 'Bidirectional' in trustDirection:
+				graph.add_edge(sourceName, targetName, color=ecolor)
+			elif 'Outbound' in trustDirection:
+				graph.add_edge(targetName, sourceName, color=ecolor)
+			elif 'Inbound' in trustDirection:
+				graph.add_edge(sourceName, targetName, color=ecolor)
+			else:
+				print(f'[-] Unrecognized relationship direction between {sourceName} and {targetName} : {trustDirection}' % (sourceName, targetName, trustDirection))
 
-            # if the source and destination domains are the same, skip
-            if (sourceName == targetName):
-                continue
+		outputFile = splitext(basename(args.trust_file))[0] + '.graphml'
+		graph.write_graph(outputFile)
 
-            if (trustType == 'MIT'):
-                # black label for MIT trusts
-                ecolor ='#000000'
-
-            else:
-                if "WITHIN_FOREST" in trustAttributes:
-                    # green label for intra-forest trusts
-                    ecolor = '#009900'
-                elif (trustAttributes == "FOREST_TRANSITIVE"):
-                    # blue label for inter-forest trusts
-                    ecolor = '#0000CC'
-                elif ((trustAttributes == "") or (trustAttributes == "TREAT_AS_EXTERNAL") or (trustAttributes == "FILTER_SIDS")):
-                    # red label for external trusts
-                    ecolor = '#FF0000'
-                else:
-                    # violet label for unknown
-                    print "[-] Unrecognized trust attributes between %s and %s : %s" % (sourceName, targetName, trustAttributes)
-                    ecolor = '#EE82EE'
-
-            # add the domain nodes to the internal graph
-            graph.add_node(sourceName, label=sourceName)
-            graph.add_node(targetName, label=targetName)
-
-            # add the edges to the graph
-            if "Bidirectional" in trustDirection:
-                graph.add_edge(sourceName, targetName, color=ecolor)
-                graph.add_edge(targetName, sourceName, color=ecolor)
-            elif "Outbound" in trustDirection:
-                graph.add_edge(targetName, sourceName, color=ecolor)
-            elif "Inbound" in trustDirection:
-                graph.add_edge(sourceName, targetName, color=ecolor)
-            else:
-                print "[-] Unrecognized relationship direction between %s and %s : %s" % (sourceName, targetName, trustDirection)
-
-        outputFile = intputFile + ".graphml"
-        nx.write_graphml(graph, outputFile)
-        print "\n[+] Graphml writte to '%s'" % (outputFile)
-        print "\n[*] Note: green = within forest, red = external, blue = forest to forest, black = MIT, violet = unrecognized\n"
+		print(f'[+] Graphml writte to "{outputFile}"')
+		print('[*] Note: green = within forest, red = external, blue = forest to forest, black = MIT, violet = unrecognized')
